c3ac156e7cfc0af861a4e635c3d34fe72ff7a21b
taekyun95 <jinhoa52@kakao.com>
2023-04-15 13:25:00 +0900

    프로젝트 생성

---
+++ src/test/kotlin/me/ktkoo/toy/ToyApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ktkoo.toy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
