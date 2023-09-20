4caaee45045680bd66017f45b6e60b61e8dfac11
taekyun95 <jinhoa52@kakao.com>
2023-09-20 18:29:42 +0900

    :recycle: 데이터 삽입 로직 변경

--- src/main/kotlin/me/ktkoo/toy/config/DatabaseInitializer.kt
+++ src/main/kotlin/me/ktkoo/toy/config/DatabaseInitializer.kt
@@ -2,56 +2,83 @@ package me.ktkoo.toy.config
 
 import java.math.BigDecimal
 import java.util.Random
-import me.ktkoo.toy.order.OrderDto
-import me.ktkoo.toy.order.OrderService
+import me.ktkoo.toy.order.Order
+import me.ktkoo.toy.order.OrderRepository
+import me.ktkoo.toy.orderproduct.OrderProduct
 import me.ktkoo.toy.orderproduct.OrderProductDto
+import me.ktkoo.toy.orderproduct.OrderProductRepository
 import me.ktkoo.toy.product.Product
 import me.ktkoo.toy.product.ProductRepository
+import me.ktkoo.toy.product.ProductService
+import mu.KotlinLogging
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class DatabaseInitializer(private val productRepository: ProductRepository, private val orderService: OrderService) :
-    CommandLineRunner {
-    val random = Random()
+class DatabaseInitializer(
+	private val productRepository: ProductRepository,
+	private val orderRepository: OrderRepository,
+	private val productService: ProductService,
+	private val orderProductRepository: OrderProductRepository
+) :
+	CommandLineRunner {
+	private val random = Random()
+	private val logger = KotlinLogging.logger {}
 
-    @Transactional
-    override fun run(vararg args: String) {
-        val products = (1..1000).map {
-            productRepository.save(
-                Product(
-                    name = "이름$it",
-                    price = BigDecimal(10_00 * it),
-                    stockQuantity = (10 * it).toLong(),
-                ),
-            )
-        }
+	@Transactional
+	override fun run(vararg args: String) {
+		val productsToSave = (1..1000).map {
+			Product(
+				name = "이름$it",
+				price = BigDecimal(10_00 * it),
+				stockQuantity = (10 * it).toLong(),
+			)
+		}
+		val savedProducts = productRepository.saveAll(productsToSave)
 
-        var orderUserId = 1L
-        val orderProductDtos = mutableListOf<OrderProductDto>()
+		var orderUserId = 1L
+		val savedOrders = mutableListOf<Order>()
+		val orderProductsToSave = mutableListOf<OrderProduct>()
 
-        for ((index, product) in products.withIndex()) {
-            product.id?.let {
-                orderProductDtos.add(
-                    OrderProductDto(
-                        productId = it,
-                        stockQuantity = random.nextLong(10),
-                    ),
-                )
-            }
+		for ((index, product) in savedProducts.withIndex()) {
+			val orderProductDtos = mutableListOf<OrderProductDto>()
 
-            if ((index + 1) % 3 == 0 || index == products.size - 1) {
-                val orderDto = OrderDto(
-                    userId = orderUserId,
-                    orderProduct = orderProductDtos,
-                )
-                orderService.createOrder(orderDto)
-                // Reset for next order
-                orderProductDtos.clear()
-                // Rotate user ID between 1 and 10
-                orderUserId = if (orderUserId < 10) orderUserId + 1 else 1
-            }
-        }
-    }
+			product.id?.let {
+				orderProductDtos.add(
+					OrderProductDto(
+						productId = it,
+						stockQuantity = random.nextLong(10),
+					)
+				)
+			}
+
+			if ((index + 1) % 3 == 0 || index == savedProducts.size - 1) {
+				val order = Order(userId = orderUserId)
+				val savedOrder = orderRepository.save(order)
+				savedOrders.add(savedOrder)
+
+				for (orderProductDto in orderProductDtos) {
+					val retrievedProduct = productService.getProduct(orderProductDto.productId)
+					retrievedProduct.order(orderProductDto.stockQuantity)
+
+					val orderProduct = OrderProduct(
+						order = savedOrder,
+						product = retrievedProduct,
+						stockQuantity = orderProductDto.stockQuantity,
+						price = retrievedProduct.price,
+					)
+					orderProductsToSave.add(orderProduct)
+				}
+
+				// Reset for next order
+				orderProductDtos.clear()
+				// Rotate user ID between 1 and 10
+				orderUserId = if (orderUserId < 10) orderUserId + 1 else 1
+			}
+		}
+
+		orderProductRepository.saveAll(orderProductsToSave)
+		logger.info { "Orders created: $savedOrders" }
+	}
 }
