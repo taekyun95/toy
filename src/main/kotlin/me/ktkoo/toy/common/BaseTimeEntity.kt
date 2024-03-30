f743810cc8d74dc7fcc2cc4e52dca317e35e5e08
taekyun95 <jinhoa52@kakao.com>
2024-03-30 13:47:01 +0900

    BaseEntity 속성 분리
    - 생성 수정 성자와 생성시간, 수정 시간이 필요없는 경우가 있어 분리

---
+++ src/main/kotlin/me/ktkoo/toy/common/BaseTimeEntity.kt
@@ -0,0 +1,23 @@
+package me.ktkoo.toy.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+open class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "created_at")
+    val createdAt: ZonedDateTime = ZonedDateTime.now()
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null
+}
