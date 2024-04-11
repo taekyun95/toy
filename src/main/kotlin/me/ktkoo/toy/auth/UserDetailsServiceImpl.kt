2a56fdcb41e9d09a86b39a1d5106d90316fefceb
taekyun95 <jinhoa52@kakao.com>
2024-04-11 18:44:43 +0900

    loadUserByUsername 메소드에 읽기용 트랜잭션 추가

--- src/main/kotlin/me/ktkoo/toy/auth/UserDetailsServiceImpl.kt
+++ src/main/kotlin/me/ktkoo/toy/auth/UserDetailsServiceImpl.kt
@@ -4,10 +4,12 @@ import me.ktkoo.toy.user.UserRepository
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
     @Throws(UsernameNotFoundException::class)
+    @Transactional(readOnly = true)
     override fun loadUserByUsername(username: String): UserDetail {
         val userDetail = userRepository.findByUsername(username)
 
